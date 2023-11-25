d13cfa926c626b09ca03cdf9201098641b83cf54
Kamillendampf <raphael@hhaerle.de>
2023-11-25 12:58:18 +0100

    4 gruppe erstellen - add layout - add color

---
+++ Frontend/BoardGameApp/app/src/main/java/com/example/boardgameapp/CreatGroup.java
@@ -0,0 +1,13 @@
+package com.example.boardgameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class creatGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creat_group);
+    }
+}
