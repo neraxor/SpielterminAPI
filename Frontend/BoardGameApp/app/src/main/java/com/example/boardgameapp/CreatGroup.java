171d5fd207f09b3bdb43896399e679b8da8f1a2b
Kamillendampf <raphael@hhaerle.de>
2023-11-26 10:03:20 +0100

    some chages

--- Frontend/BoardGameApp/app/src/main/java/com/example/boardgameapp/CreatGroup.java
+++ Frontend/BoardGameApp/app/src/main/java/com/example/boardgameapp/CreatGroup.java
@@ -3,7 +3,7 @@ package com.example.boardgameapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class creatGroup extends AppCompatActivity {
+public class CreatGroup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
